99f6ee7fd98474e24635a79ffb9ec65a0131ca0e
Talita Lima <talita.lima.tech@gmail.com>
2026-03-16 21:45:49 -0300

    feat: setup database connection with sequelize and mysql

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,16 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config(); // Carrega as senhas do arquivo .env
+require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME, // Ex: store_api
-    process.env.DB_USER, // Geralmente 'root'
-    process.env.DB_PASS, // Sua senha do MySQL
-    {
-        host: process.env.DB_HOST, // 'localhost'
-        dialect: 'mysql',
-        logging: false, // Para não encher o terminal de textos 
-        define: {
-            timestamps: true, // Cria o created_at e updated_at automaticamente
-            underscored: true // Transforma createdAt em created_at
-        }
-    }
-);
-
-module.exports = sequelize;
+module.exports = {
+  dialect: process.env.DB_DIALECT || 'mysql',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
+  port: 3306, // Porta padrão do MySQL
+  logging: false, 
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
